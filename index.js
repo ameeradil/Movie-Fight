a98f9992bcc96425859c7f4b775aa691e8c8abea
AMEER <ameer.ad.eltaib@gmail.com>
2026-03-05 15:25:26 +0545

    Update API URL from HTTP to HTTPS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const autocompleteConfig ={
         return movie.Title
     },
     async fetchData(searchTerm){
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: API_KEY,
             s: searchTerm
@@ -49,7 +49,7 @@ createAutoComplete({
 let leftMovie
 let rightMovie
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await await axios.get('http://www.omdbapi.com/', {
+    const response = await await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: API_KEY,
             i: movie.imdbID
